f50eea4c60da3beca6524fec1db3dec270a4272f
Iman Ibrahim <ibra0198@algonquinlive.com>
2019-04-04 12:47:29 -0400

    adding mapview component

---
+++ src/components/shared/map-view/map-view.component.js
@@ -0,0 +1,36 @@
+import {
+  gmapApi
+} from 'vue2-google-maps'
+
+export default {
+  name: 'mapView',
+  props: {
+    showlist: Boolean,
+    addressList: Array
+  },
+  data() {
+    return {
+      markers: []
+    }
+  },
+  computed: {
+    google: gmapApi,
+  },
+  mounted() {
+
+  },
+  methods: {
+    initPlaces() {
+      this.markers = this.addressList.map(address => {
+        const marker = {
+          lat: address.lat,
+          lng: address.lng
+        };
+
+        return {
+          position: marker
+        };
+      });
+    }
+  }
+}
